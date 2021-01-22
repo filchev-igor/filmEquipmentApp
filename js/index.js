936c1000ea6fae378fe3f943ce1b48fa0809256f
Igor Filchev <filchev.igor@gmail.com>
2021-01-22 04:21:39 +0200

    Code for controlling the page and text color were added.

--- js/index.js
+++ js/index.js
@@ -25,20 +25,31 @@ class SettingsToggleSwitch {
         }
 
         const switchElementsId = settingsInput["switchElements"]["suppliers"];
+        const switchConsumersId = settingsInput["switchElements"]["consumers"];
 
         for (const switchElementId of switchElementsId) {
             const switchElement = document.getElementById(switchElementId);
 
-            //const checked = switchElement.checked;
             const switchElementLabel = switchElement.labels[0];
 
             const [numberOrder, tag,] = switchElementId.split(/(?=[A-Z])/);
 
-            const switchConsumersId = settingsInput["switchElements"]["consumers"];
             const switchConsumerId = switchConsumersId.filter(value => value.includes(numberOrder + tag))
                 .join();
 
             switchElement.addEventListener("click", () => this.#handleNavbarColorSchemes([numberOrder, switchElement, switchConsumerId]));
+        }
+
+        const textToggleElementName = settingsInput["textOnPage"]["supplier"];
+        const textConsumerElementId = settingsInput["textOnPage"]["consumer"];
+
+        const textToggleElements = document.getElementsByName(textToggleElementName);
+
+        for (const pageToggleElement of textToggleElements) {
+            const pageToggleElementLabel = pageToggleElement.labels[0];
+            const textColor = pageToggleElement.dataset.textColor;
+
+            pageToggleElementLabel.addEventListener("click", () => this.#handleTextColorToggle([textColor, textConsumerElementId]));
         }
     }
 
@@ -88,32 +99,57 @@ class SettingsToggleSwitch {
         consumerElement.classList.remove(removeClass);
         consumerElement.classList.add(addClass);
     }
+
+    #handleTextColorToggle(props) {
+        const [textColor, textConsumerElementId] = props;
+
+        const element = document.querySelector("#" + textConsumerElementId + " p");
+
+        const removableClass = element.className
+            .split(" ")
+            .filter(value => value.includes("text-"))
+            .join();
+
+        if (removableClass !== "")
+            element.classList.remove(removableClass);
+
+        element.classList.add(textColor);
+    }
 }
 
 const NAVBAR_BG_RADIO_NAME = "navbarColorPicker";
 const FOOTER_BG_RADIO_NAME = "footerColorPicker";
+const PAGE_BG_RADIO_NAME = "pageColorPicker";
 
 const FIRST_NAVBAR_CONSUMER_ID = "firstNavbarConsumer";
 const SECOND_NAVBAR_CONSUMER_ID = "secondNavbarConsumer";
 
-const FOOTER_CONSUMER_ID = "footerColorToggleConsumer";
+const FOOTER_CONSUMER_ID = "footerConsumer";
+
+const PAGE_CONSUMER_ID = "pageConsumer";
 
 const FIRST_NAVBAR_COLOR_SCHEME_SWITCHER = "firstNavbarColorScheme";
 const SECOND_NAVBAR_COLOR_SCHEME_SWITCHER = "secondNavbarColorScheme";
 
+const TEXT_COLOR_RADIO_NAME = "textColorPicker";
 
 
 const settingsInput = {
     "radioElements": {
-        "suppliers": [NAVBAR_BG_RADIO_NAME, FOOTER_BG_RADIO_NAME],
+        "suppliers": [NAVBAR_BG_RADIO_NAME, FOOTER_BG_RADIO_NAME, PAGE_BG_RADIO_NAME],
         "consumers": {
             "navbar": [FIRST_NAVBAR_CONSUMER_ID, SECOND_NAVBAR_CONSUMER_ID],
-            "footer": [FOOTER_CONSUMER_ID]
+            "footer": [FOOTER_CONSUMER_ID],
+            "page": [PAGE_CONSUMER_ID]
         }
     },
     "switchElements": {
         "suppliers": [FIRST_NAVBAR_COLOR_SCHEME_SWITCHER, SECOND_NAVBAR_COLOR_SCHEME_SWITCHER],
         "consumers": [FIRST_NAVBAR_CONSUMER_ID, SECOND_NAVBAR_CONSUMER_ID]
+    },
+    "textOnPage": {
+        "supplier": TEXT_COLOR_RADIO_NAME,
+        "consumer": PAGE_CONSUMER_ID
     }
 };
 
