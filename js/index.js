8dc88d0d5dca8d10d1095a6c2a94a5c4e6e41966
Igor Filchev <filchev.igor@gmail.com>
2021-01-22 03:15:32 +0200

    Color picker code was simplified. Navbar color schemes switcher was added.

--- js/index.js
+++ js/index.js
@@ -14,49 +14,106 @@ class SettingsToggleSwitch {
 
             const [handledElementsName,,] = radioElementsName.split(/(?=[A-Z])/);
 
-            let handledElements = document.getElementsByClassName(handledElementsName);
-
-            if (!handledElements.length) {
-                const id = settingsInput["radioElements"]["consumers"]["footerConsumer"];
-
-                handledElements = document.querySelectorAll("#" + id);
-            }
+            const handledElementsId = settingsInput["radioElements"]["consumers"][handledElementsName];
 
             for (const radioElement of radioElements) {
-                const color = radioElement.dataset.bgColor;
+                const bgColor = radioElement.dataset.bgColor;
+                const elementLabel = radioElement.labels[0];
 
-                radioElement.labels[0].addEventListener("click", () => this.#handleColorToggle(color, handledElements));
+                elementLabel.addEventListener("click", () => this.#handleBgColorToggle([bgColor, handledElementsId]));
             }
+        }
+
+        const switchElementsId = settingsInput["switchElements"]["suppliers"];
+
+        for (const switchElementId of switchElementsId) {
+            const switchElement = document.getElementById(switchElementId);
+
+            //const checked = switchElement.checked;
+            const switchElementLabel = switchElement.labels[0];
+
+            const [numberOrder, tag,] = switchElementId.split(/(?=[A-Z])/);
+
+            const switchConsumersId = settingsInput["switchElements"]["consumers"];
+            const switchConsumerId = switchConsumersId.filter(value => value.includes(numberOrder + tag))
+                .join();
+
+            switchElement.addEventListener("click", () => this.#handleNavbarColorSchemes([numberOrder, switchElement, switchConsumerId]));
         }
     }
 
-    #handleColorToggle(color, handledElements) {
-        for (const handledElement of handledElements) {
-            const elementClassList = handledElement.className
-                .split(" ")
-                .filter(value => value.includes("bg-") && value !== "bg-gradient");
+    #handleBgColorToggle(props) {
+        const [bgColor, handledElementsId] = props;
+        for (const handledElementId of handledElementsId) {
+            const element = document.getElementById(handledElementId);
 
-            handledElement.classList.remove(elementClassList[0]);
-            handledElement.classList.add(color);
+            const removableClass = element.className
+                .split(" ")
+                .filter(value => value.includes("bg-") && value !== "bg-gradient")
+                .join();
+
+            element.classList.remove(removableClass);
+            element.classList.add(bgColor);
         }
+    }
+
+    #handleNavbarColorSchemes(props) {
+        const [numberOrder, switchElement, switchConsumerId] = props;
+
+        const consumerElement = document.getElementById(switchConsumerId);
+
+        const checked = switchElement.checked;
+
+        let colorScheme;
+        let removeClass = "";
+        let addClass = "";
+
+        if (checked) {
+            colorScheme = "dark";
+
+            removeClass = "navbar-light";
+            addClass = "navbar-dark";
+        }
+        else {
+            colorScheme = "light";
+
+            removeClass = "navbar-dark";
+
+            if (numberOrder !== "first")
+                addClass = "navbar-light";
+        }
+
+        switchElement.innerText = "first navbar" + colorScheme + "scheme";
+
+        consumerElement.classList.remove(removeClass);
+        consumerElement.classList.add(addClass);
     }
 }
 
 const NAVBAR_BG_RADIO_NAME = "navbarColorPicker";
 const FOOTER_BG_RADIO_NAME = "footerColorPicker";
 
+const FIRST_NAVBAR_CONSUMER_ID = "firstNavbarConsumer";
+const SECOND_NAVBAR_CONSUMER_ID = "secondNavbarConsumer";
+
 const FOOTER_CONSUMER_ID = "footerColorToggleConsumer";
 
-//const navbarColorPicker = document.querySelectorAll("#navbarColorPicker > .bg-gradient");
-//const footerColorPicker = document.querySelectorAll("#footerColorPicker > .bg-gradient");
+const FIRST_NAVBAR_COLOR_SCHEME_SWITCHER = "firstNavbarColorScheme";
+const SECOND_NAVBAR_COLOR_SCHEME_SWITCHER = "secondNavbarColorScheme";
 
-//const settingsInput = [navbarColorPicker, footerColorPicker];
+
+
 const settingsInput = {
     "radioElements": {
         "suppliers": [NAVBAR_BG_RADIO_NAME, FOOTER_BG_RADIO_NAME],
         "consumers": {
-            "footerConsumer": FOOTER_CONSUMER_ID
+            "navbar": [FIRST_NAVBAR_CONSUMER_ID, SECOND_NAVBAR_CONSUMER_ID],
+            "footer": [FOOTER_CONSUMER_ID]
         }
+    },
+    "switchElements": {
+        "suppliers": [FIRST_NAVBAR_COLOR_SCHEME_SWITCHER, SECOND_NAVBAR_COLOR_SCHEME_SWITCHER],
+        "consumers": [FIRST_NAVBAR_CONSUMER_ID, SECOND_NAVBAR_CONSUMER_ID]
     }
 };
 
