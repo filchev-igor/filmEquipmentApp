5168b85c5eb11697160b07e29287f58e3a003a7b
Igor Filchev <filchev.igor@gmail.com>
2021-01-22 07:56:55 +0200

    Small changes

--- js/index.js
+++ js/index.js
@@ -1,145 +1,10 @@
 'use strict';
 
 import UpdateCopyright from "../js/updateCopyright.js";
+import StyleSettings from "../js/styleSettings.js";
 
 const updateCopyright = new UpdateCopyright("copyrightEnd");
 updateCopyright.updateField();
-
-class SettingsToggleSwitch {
-    constructor(settingsInput) {
-        const radioElementsNames = settingsInput["radioElements"]["suppliers"];
-
-        for (const radioElementsName of radioElementsNames) {
-            const radioElements = document.getElementsByName(radioElementsName);
-
-            const [handledElementsName,,] = radioElementsName.split(/(?=[A-Z])/);
-
-            const handledElementsId = settingsInput["radioElements"]["consumers"][handledElementsName];
-
-            for (const radioElement of radioElements) {
-                const bgColor = radioElement.dataset.bgColor;
-                const elementLabel = radioElement.labels[0];
-
-                elementLabel.addEventListener("click",
-                    () => this.#handleBgColorToggle([bgColor, handledElementsId])
-                );
-            }
-        }
-
-        const switchElementsId = settingsInput["switchElements"]["suppliers"];
-        const switchConsumersId = settingsInput["switchElements"]["consumers"];
-
-        for (const switchElementId of switchElementsId) {
-            const switchElement = document.getElementById(switchElementId);
-
-            const switchElementLabel = switchElement.labels[0];
-
-            const [numberOrder, tag,] = switchElementId.split(/(?=[A-Z])/);
-
-            const switchConsumerId = switchConsumersId.filter(value => value.includes(numberOrder + tag))
-                .join();
-
-            switchElement.addEventListener("click",
-                () => this.#handleNavbarColorSchemes([numberOrder, switchElement, switchConsumerId])
-            );
-        }
-
-        const textToggleElementName = settingsInput["textOnPage"]["color"]["supplier"];
-        const textConsumerElementId = settingsInput["textOnPage"]["color"]["consumer"];
-
-        const textToggleElements = document.getElementsByName(textToggleElementName);
-
-        for (const pageToggleElement of textToggleElements) {
-            const pageToggleElementLabel = pageToggleElement.labels[0];
-            const textColor = pageToggleElement.dataset.textColor;
-
-            pageToggleElementLabel.addEventListener("click",
-                () => this.#handleTextColorToggle([textColor, textConsumerElementId])
-            );
-        }
-
-        const textSizeSupplierId = settingsInput["textOnPage"]["size"]["supplier"];
-        const textSizeConsumerId = settingsInput["textOnPage"]["size"]["consumer"];
-
-        const textSizeSupplier = document.getElementById(textSizeSupplierId);
-
-        textSizeSupplier.addEventListener("input",
-            (e) => this.#handleTextSize([textSizeSupplier, textSizeConsumerId])
-        );
-    }
-
-    #handleBgColorToggle(props) {
-        const [bgColor, handledElementsId] = props;
-        for (const handledElementId of handledElementsId) {
-            const element = document.getElementById(handledElementId);
-
-            const removableClass = element.className
-                .split(" ")
-                .filter(value => value.includes("bg-") && value !== "bg-gradient")
-                .join();
-
-            element.classList.remove(removableClass);
-            element.classList.add(bgColor);
-        }
-    }
-
-    #handleNavbarColorSchemes(props) {
-        const [numberOrder, switchElement, switchConsumerId] = props;
-
-        const consumerElement = document.getElementById(switchConsumerId);
-
-        const checked = switchElement.checked;
-
-        let colorScheme;
-        let removeClass = "";
-        let addClass = "";
-
-        if (checked) {
-            colorScheme = "dark";
-
-            removeClass = "navbar-light";
-            addClass = "navbar-dark";
-        }
-        else {
-            colorScheme = "light";
-
-            removeClass = "navbar-dark";
-
-            if (numberOrder !== "first")
-                addClass = "navbar-light";
-        }
-
-        switchElement.innerText = "first navbar" + colorScheme + "scheme";
-
-        consumerElement.classList.remove(removeClass);
-        consumerElement.classList.add(addClass);
-    }
-
-    #handleTextColorToggle(props) {
-        const [textColor, textConsumerElementId] = props;
-
-        const element = document.querySelector("#" + textConsumerElementId + " p");
-
-        const removableClass = element.className
-            .split(" ")
-            .filter(value => value.includes("text-"))
-            .join();
-
-        if (removableClass !== "")
-            element.classList.remove(removableClass);
-
-        element.classList.add(textColor);
-    }
-
-    #handleTextSize(props) {
-        const [textSizeSupplier, textSizeConsumerId] = props;
-
-        const element = document.querySelector("#" + textSizeConsumerId + " p");
-        element.style.fontSize = textSizeSupplier.value + "rem";
-
-        console.log(textSizeSupplier.value + "rem");
-    }
-}
 
 const NAVBAR_BG_RADIO_NAME = "navbarColorPicker";
 const FOOTER_BG_RADIO_NAME = "footerColorPicker";
@@ -184,7 +49,18 @@ const settingsInput = {
     },
 };
 
-const settingsToggleSwitch = new SettingsToggleSwitch(settingsInput);
+new StyleSettings(settingsInput);
+
+const toggleActiveLink = () => {
+    const path = window.location.pathname;
+    const page = path.split("/").pop();
+
+    const element = document.querySelectorAll("a[href='" + page + "']");
+
+    element[0].classList.add("active");
+    element[0].setAttribute("aria-current", "page");
+}
+
+toggleActiveLink();
 
 //TODO update titles of all pages (they have default name)
-//TODO add button with setup via JS
